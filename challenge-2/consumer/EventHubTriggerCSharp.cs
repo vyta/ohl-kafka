71e7fd0522fa42650a7c4c2e4b77027e622d731e
Vy Ta <vyta@microsoft.com>
2019-05-10 14:38:44 -0700

    Function stuffs

--- challenge-2/consumer/EventHubTriggerCSharp.cs
+++ challenge-2/consumer/EventHubTriggerCSharp.cs
@@ -6,24 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace T8.Consumer
 {
     public static class EventHubTriggerCSharp
     {
         [FunctionName("EventHubTriggerCSharp")]
-        public static async Task Run([EventHubTrigger("payment", Connection = "SourceEventHub")] EventData[] events, ILogger log)
+        public static async Task Run([EventHubTrigger("payment", Connection = "SourceEventHub")] EventData[] events, ILogger log, 
+        [EventHub("outputPayments", Connection = "SourceEventHub")] IAsyncCollector<EventData> output)
         {
             var exceptions = new List<Exception>();
-
+            
             foreach (EventData eventData in events)
             {
                 try
                 {
                     string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
 
+                    var payment = JObject.Parse(messageBody);
+
+                    payment["timeProcessed"] = DateTime.Now.ToString();
+                    payment["paymentValid"] = "1";
+
+                    await output.AddAsync(new EventData(Encoding.UTF8.GetBytes(payment.ToString())));
                     // Replace these two lines with your processing logic.
-                    log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
+                    log.LogInformation($"C# Event Hub trigger function processed a message: {payment}");
                     await Task.Yield();
                 }
                 catch (Exception e)
@@ -41,6 +50,7 @@ namespace T8.Consumer
 
             if (exceptions.Count == 1)
                 throw exceptions.Single();
+
         }
     }
 }
